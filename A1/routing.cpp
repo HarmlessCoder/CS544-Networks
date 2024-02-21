d97008a5d2a3f2156ca052177e977a6ae94c9d55
Gaurav <gauravmahaur2@gmail.com>
2024-02-22 02:51:54 +0530

    INQ algorithm correction

--- A1/routing.cpp
+++ A1/routing.cpp
@@ -4,7 +4,8 @@
 #include <random>
 #include <fstream>
 #include <cmath>
-
+#include <utility>
+using namespace std;
 // Packet structure
 struct Packet {
     int src_port;
@@ -18,16 +19,16 @@ private:
     int num_ports;
     int buffer_size;
     double packet_gen_prob;
-    std::string queue_type;
+    string queue_type;
     int max_time_slots;
 
     // Queues for each port
-    std::vector<std::queue<Packet>> input_queues;
-    std::vector<std::queue<Packet>> output_queues;
+    vector<queue<Packet>> input_queues;
+    vector<queue<Packet>> output_queues;
 
     // Random number generator
-    std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution;
+    default_random_engine generator;
+    uniform_real_distribution<double> distribution;
 
     // Metrics
     double total_packet_delay;
@@ -50,17 +51,31 @@ private:
 
     void schedulePackets() {
         if (queue_type == "INQ") {
-            for (int src_port = 0; src_port < num_ports; ++src_port) {
-                while (!input_queues[src_port].empty()) {
+            // for (int src_port = 0; src_port < num_ports; ++src_port) {
+            //     while (!input_queues[src_port].empty()) {
+            //         Packet packet = input_queues[src_port].front();
+            //         input_queues[src_port].pop();
+            //         if (output_queues[packet.dest_port].empty()) {
+            //             output_queues[packet.dest_port].push(packet);
+            //         } else {
+            //             // Randomly select one packet for transmission
+            //             int rand_index = distribution(generator) * output_queues[packet.dest_port].size();
+            //             output_queues[packet.dest_port].push(packet);
+            //         }
+            //     }
+            // }
+            vector<vector<Packet>>temp(num_ports);
+            for(int src_port = 0;src_port<num_ports;++src_port){
+                if(!input_queues[src_port].empty()){
                     Packet packet = input_queues[src_port].front();
-                    input_queues[src_port].pop();
-                    if (output_queues[packet.dest_port].empty()) {
-                        output_queues[packet.dest_port].push(packet);
-                    } else {
-                        // Randomly select one packet for transmission
-                        int rand_index = distribution(generator) * output_queues[packet.dest_port].size();
-                        output_queues[packet.dest_port].push(packet);
-                    }
+                    temp[packet.dest_port].push_back(packet);
+                }
+            }
+            for(int dest_port=0;dest_port<num_ports;++dest_port){
+                if(temp[dest_port].size()){
+                    int ind=rand()%temp[dest_port].size();
+                    output_queues[dest_port].push(temp[dest_port][ind]);
+                    input_queues[temp[dest_port][ind].src_port].pop();
                 }
             }
         } else if (queue_type == "KOUQ") {
@@ -77,7 +92,7 @@ private:
                 }
             }
         } else if (queue_type == "iSLIP") {
-            std::vector<int> grant(num_ports, -1);
+            vector<int> grant(num_ports, -1);
             for (int r = 0; r < num_ports; ++r) {
                 for (int i = 0; i < num_ports; ++i) {
                     if (input_queues[i].empty()) continue;
@@ -117,7 +132,7 @@ private:
     }
 
 public:
-    Switch(int switch_port_count, int buffer_size, double packet_gen_prob, std::string queue_type, int max_time_slots)
+    Switch(int switch_port_count, int buffer_size, double packet_gen_prob, string queue_type, int max_time_slots)
         : num_ports(switch_port_count), buffer_size(buffer_size), packet_gen_prob(packet_gen_prob), 
         queue_type(queue_type), max_time_slots(max_time_slots), distribution(0.0, 1.0) {
         input_queues.resize(num_ports);
@@ -133,8 +148,8 @@ public:
         double avg_packet_delay = total_packet_delay / total_transmitted_packets;
         double avg_link_utilization = total_link_utilization / (num_ports * max_time_slots);
         double kouq_drop_probability = static_cast<double>(total_dropped_packets) / (num_ports * max_time_slots);
-        std::cout << num_ports << "\t" << packet_gen_prob << "\t" << queue_type << "\t" 
-                  << avg_packet_delay << "\t" << avg_link_utilization << "\t" << kouq_drop_probability << std::endl;
+        cout << num_ports << "\t" << packet_gen_prob << "\t" << queue_type << "\t" 
+                  << avg_packet_delay << "\t" << avg_link_utilization << "\t" << kouq_drop_probability << endl;
     }
 };
 
@@ -143,21 +158,21 @@ int main(int argc, char *argv[]) {
     int switch_port_count = 8; // Default value
     int buffer_size = 4; // Default value
     double packet_gen_prob = 0.5; // Default value
-    std::string queue_type = "INQ"; // Default value
+    string queue_type = "INQ"; // Default value
     int max_time_slots = 10000; // Default value
 
     for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
+        string arg = argv[i];
         if (arg == "-N") {
-            switch_port_count = std::stoi(argv[++i]);
+            switch_port_count = stoi(argv[++i]);
         } else if (arg == "-B") {
-            buffer_size = std::stoi(argv[++i]);
+            buffer_size = stoi(argv[++i]);
         } else if (arg == "-p") {
-            packet_gen_prob = std::stod(argv[++i]);
+            packet_gen_prob = stod(argv[++i]);
         } else if (arg == "-queue") {
             queue_type = argv[++i];
         } else if (arg == "-T") {
-            max_time_slots = std::stoi(argv[++i]);
+            max_time_slots = stoi(argv[++i]);
         }
     }
 
---
+++ A1/temp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>a(10);
+    int x=rand()%a.size();
+    int y=rand()%a.size();
+    cout<<x<<endl;
+    cout<<y<<endl;
+}
